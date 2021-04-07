bf79bf5e90bfdfa511e3a053fc5e27ace8e499c7
Philip Kempton <p_kempton@hotmail.com>
2021-04-07 15:37:36 -0400

    Added validation for Task Summary and Task Description

--- Contracts/Task.cs
+++ Contracts/Task.cs
@@ -1,14 +1,18 @@
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Tasklify.Contracts
 {
-    public class TasklifyTask
+    public class TasklifyTask :IValidatableObject
     {
         [JsonProperty("id", NullValueHandling=NullValueHandling.Ignore)]
         public int Id {get; private set;}
         [JsonProperty("summary")]
+        [Required, StringLength(100, ErrorMessage = "{0} cannot exceed {1} characters")]
         public string Summary {get; set;}
         [JsonProperty("description", NullValueHandling=NullValueHandling.Ignore)]
+        [StringLength(500, ErrorMessage = "{0} cannot exceed {1} characters")]
         public string Description {get; set;}
 
         public TasklifyTask(int id, string summary, string description)
@@ -19,6 +23,12 @@ namespace Tasklify.Contracts
                 Summary = summary;
                 Description = description;
             }
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var results = new List<ValidationResult>();
+            return results;
         }
     }
 }
